988533dd20b18ccac3a86658b60fa3c4bd185eec
Dana Alibrandi <dalibrandi@gmail.com>
2018-08-25 18:02:00 +0700

    added projects page

--- server.js
+++ server.js
@@ -50,6 +50,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Project Page',
+    welcomeMsg: 'Projects will go here'
+  });
+});
 
 app.get('/bad',(req, res) => {
   res.send({
